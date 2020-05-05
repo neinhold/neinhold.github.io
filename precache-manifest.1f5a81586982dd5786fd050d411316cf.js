6ae9d0e2845bb52d8469146d55d6789f5437a97f
Neinhold <11391206+neinhold@users.noreply.github.com>
2020-05-05 18:50:50 +0200

    Updates

---
+++ precache-manifest.1f5a81586982dd5786fd050d411316cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6a130d26fa507ce63a92e1fb95c39a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1bb8f551cbe1323adf5",
+    "url": "/static/css/main.c3cf636e.chunk.css"
+  },
+  {
+    "revision": "7bee091cb9ea5a24df17",
+    "url": "/static/js/2.68e79f9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.68e79f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1bb8f551cbe1323adf5",
+    "url": "/static/js/main.fcd21739.chunk.js"
+  },
+  {
+    "revision": "aad629e9f18b5d535c3c",
+    "url": "/static/js/runtime-main.0eb32bfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
